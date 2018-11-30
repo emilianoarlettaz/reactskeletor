53b33f91e0ab65e8a1017ab08e902911e2244c4e
emilianoarlettaz <emilianoarlettaz@gmail.com>
2018-11-30 00:00:32 -0300

    snapshot tests with jest added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = ({ title }) => <div>{title}</div>
+export const doIncrement = prevState => ({
+  counter: prevState.counter + 1,
+});
+
+export const doDecrement = prevState => ({
+  counter: prevState.counter - 1,
+});
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState(doIncrement);
+  }
+
+  onDecrement() {
+    this.setState(doDecrement);
+  }
+
+  render() {
+    const { title } = this.props;
+    const { counter } = this.state;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <Counter counter={counter} />
+
+        <button
+          type="button"
+          onClick={this.onIncrement}
+        >
+          Increment
+        </button>
+
+        <button
+          type="button"
+          onClick={this.onDecrement}
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
+}
+
+export const Counter = ({ counter }) => <p>{counter}</p>;
 
 App.propTypes = {
   title: PropTypes.string.isRequired,
+};
+
+Counter.propTypes = {
+  counter: PropTypes.number.isRequired,
 };
 
 export default App;
---
+++ src/App.snapshot.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App, { Counter } from './App';
+
+describe('App Snapshot', () => {
+  const title = 'Testing App';
+
+  test('renders', () => {
+    const component = renderer.create(
+      <App title={title} />,
+    );
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Counter Snapshot', () => {
+  test('renders', () => {
+    const component = renderer.create(
+      <Counter counter={1} />,
+    );
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
